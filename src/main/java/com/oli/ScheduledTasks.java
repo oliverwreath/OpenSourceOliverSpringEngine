661168ade2dda95f3cbd5c6a1d9b1a4aab923f03
oliverwreath <skywalkerhunter@gmail.com>
2019-04-26 00:16:50 -0400

    turn off scheduled tasks.

--- src/main/java/com/oli/ScheduledTasks.java
+++ src/main/java/com/oli/ScheduledTasks.java
@@ -56,11 +56,11 @@ public class ScheduledTasks {
 //        updateJoinTables_with_EmployeeName();
     }
 
-    @Scheduled(fixedDelay = ONE_SECOND, initialDelay = ONE_SECOND)
-    public void scheduleFixedRateWithInitialDelayTask() {
-        long now = System.currentTimeMillis() / 1000;
-        System.out.println("Fixed rate task with one second initial delay - " + now);
-    }
+//    @Scheduled(fixedDelay = ONE_SECOND, initialDelay = ONE_SECOND)
+//    public void scheduleFixedRateWithInitialDelayTask() {
+//        long now = System.currentTimeMillis() / 1000;
+//        System.out.println("Fixed rate task with one second initial delay - " + now);
+//    }
 
 //    /**
 //     * Don't touch the code, important yet delicate:
