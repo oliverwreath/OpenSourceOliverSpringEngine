9bfe3fbe8b851e746de26ef9d4aab6096d818256
oliverwreath <skywalkerhunter@gmail.com>
2019-04-28 04:10:38 -0400

    when register, send email with confirmation code.

--- src/main/java/com/oli/Controller/RegisterController.java
+++ src/main/java/com/oli/Controller/RegisterController.java
@@ -1,5 +1,6 @@
 package com.oli.Controller;
 
+import com.oli.EmailCfg;
 import com.oli.Entities.Authority;
 import com.oli.Entities.DropDownListEntities;
 import com.oli.Entities.Member;
@@ -7,15 +8,20 @@ import com.oli.Entities.Utils.EnumsUtils;
 import com.oli.Services.DropDownListEntityService;
 import com.oli.Services.MemberService;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.lang3.Validate;
 import org.springframework.http.HttpStatus;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSenderImpl;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.server.ResponseStatusException;
 
 import javax.validation.Valid;
+import javax.validation.constraints.Email;
 import java.util.*;
 
 import static com.oli.Controller.ControllerUtils.REDIRECT_PREFIX;
@@ -29,10 +35,43 @@ import static com.oli.SecurityConfig.passwordEncoder;
 public class RegisterController {
     private final DropDownListEntityService service;
     private final MemberService memberService;
+    private final EmailCfg emailCfg;
 
-    public RegisterController(DropDownListEntityService service, MemberService memberService) {
+    public RegisterController(DropDownListEntityService service, MemberService memberService, EmailCfg emailCfg) {
         this.service = service;
         this.memberService = memberService;
+        this.emailCfg = emailCfg;
+    }
+
+    @PostMapping(value = "/GetConfirmationCode")
+    public String getConfirmationCode(@Valid @Email @RequestParam("username") String userName) {
+        // filter abnormal inputs
+        Validate.notNull(userName);
+        log.debug("userName = {}", userName);
+
+        log.debug("emailCfg = {}", emailCfg);
+        // Create a mail sender
+        JavaMailSenderImpl mailSender = new JavaMailSenderImpl();
+        mailSender.setHost(this.emailCfg.getHost());
+        mailSender.setPort(this.emailCfg.getPort());
+        mailSender.setUsername(this.emailCfg.getUsername());
+        mailSender.setPassword(this.emailCfg.getPassword());
+        Properties props = mailSender.getJavaMailProperties();
+        props.put("mail.transport.protocol", "smtp");
+        props.put("mail.smtp.auth", "true");
+        props.put("mail.smtp.starttls.enable", "true");
+        props.put("mail.debug", "true");
+        log.debug("mailSender = {}", mailSender);
+        // Create an email instance
+        SimpleMailMessage mailMessage = new SimpleMailMessage();
+        mailMessage.setFrom(this.emailCfg.getFrom());
+        mailMessage.setTo(userName);
+        mailMessage.setSubject("New feedback from " + this.emailCfg.getFrom());
+        mailMessage.setText("Text success!");
+        log.debug("mailMessage = {}", mailMessage);
+        // Send mail
+        mailSender.send(mailMessage);
+        return REDIRECT_PREFIX + "/signup?info=Please check email for confirmation code!";
     }
 
     @GetMapping(value = "/register")
---
+++ src/main/java/com/oli/EmailCfg.java
@@ -0,0 +1,25 @@
+package com.oli;
+
+import lombok.Data;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+/**
+ * Author: Oliver
+ */
+@Slf4j
+@Component
+@Data
+public class EmailCfg {
+    @Value("${spring.mail.host}")
+    private String host;
+    @Value("${spring.mail.port}")
+    private int port;
+    @Value("${spring.mail.username}")
+    private String username;
+    @Value("${spring.mail.password}")
+    private String password;
+    @Value("${spring.mail.from}")
+    private String from;
+}
